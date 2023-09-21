89a9940903a517c76066e54733b1034980629a0c
LeeHueeng <zzxx373014@gmail.com>
2023-09-21 09:27:48 +0900

    [#2] 이미지 박스 추가

--- src/component/DayWebtoon.tsx
+++ src/component/DayWebtoon.tsx
@@ -1,5 +1,16 @@
 import React from "react";
-import { DayWebtoon } from "../styled/DayWebtoon";
+import { DayWebtoonStyle } from "../styled/DayWebtoon";
+
 export default function DayWebtoon() {
-  return <div>DayWebtoon</div>;
+  return (
+    <div>
+      <DayWebtoonStyle>
+        <img src="/webtoon.jpg" alt="day webtoon" />
+        <img src="/webtoon.jpg" alt="day webtoon" />
+        <img src="/webtoon.jpg" alt="day webtoon" />
+        <img src="/webtoon.jpg" alt="day webtoon" />
+        <img src="/webtoon.jpg" alt="day webtoon" />
+      </DayWebtoonStyle>
+    </div>
+  );
 }
--- src/styled/DayWebtoon.tsx
+++ src/styled/DayWebtoon.tsx
@@ -1,5 +1,21 @@
 import styled from "styled-components";
 
 export const DayWebtoonStyle = styled.div`
-  /* 스타일 코드 작성 */
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px #999999;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  img {
+    width: 158px;
+    height: 205px;
+    border: 1px solid #999999;
+  }
 `;
