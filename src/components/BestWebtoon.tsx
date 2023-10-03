4092e637e74b2834d4d42a974bb4e9eaa72d7cc4
LeeHueeng <zzxx373014@gmail.com>
2023-10-03 18:00:03 +0900

    [#2] 우측 웹툰리스트 css 수정

--- src/components/BestWebtoon.tsx
+++ src/components/BestWebtoon.tsx
@@ -1,6 +1,59 @@
 import React from "react";
+import styled from "styled-components";
+import { BestWebtoonmainStyle } from "../styles/BestWebtoonStyle";
 
 export default function BestWebtoon() {
-  return <div>Best</div>;
+  return (
+    <div style={{ width: "100%" }}>
+      <BestWebtoonmainStyle>
+        <div>
+          <h2
+            style={{
+              fontSize: "20px",
+              lineHeight: "21px",
+              fontWeight: 600,
+              letterSpacing: "-0.5px",
+              color: "black",
+            }}
+          >
+            실시간 인기웹툰
+          </h2>
+        </div>
+        <div>
+          <button
+            style={{
+              border: "0",
+              background: "rgba(0,0,0,0)",
+              outline: "none",
+              cursor: "pointer",
+            }}
+          >
+            전체
+          </button>
+
+          <button
+            style={{
+              border: "0",
+              background: "rgba(0,0,0,0)",
+              outline: "none",
+              cursor: "pointer",
+            }}
+          >
+            여성
+          </button>
+
+          <button
+            style={{
+              border: "0",
+              background: "rgba(0,0,0,0)",
+              outline: "none",
+              cursor: "pointer",
+            }}
+          >
+            남성
+          </button>
+        </div>
+      </BestWebtoonmainStyle>
+    </div>
+  );
 }
-fire event 
