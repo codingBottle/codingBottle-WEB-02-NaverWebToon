bf62e9f32375afc1ec5ea6dd492012f7d3b9e293
yeeZinu <doglife222@gmail.com>
2023-10-18 02:34:44 +0900

    add(#4): 상단 Nav바 구성요소 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import DayWebtoon from "./components/DayWebtoon";
 import BestWebtoon from "./components/BestWebtoon";
 import "./App.css";
+import WebtoonNav from "./components/WebtoonNav";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <WebtoonNav />
 
       <div className="Content_wrap">
         <div className="Leftbox">
